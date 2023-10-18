5039d5adb5919351d77a59f592dd2a3ed6f7abf9
A8250092 <stanley.mei08@gmail.com>
2023-10-18 08:12:01 +0800

    Update Examples and Counterexamples.py

--- week4/Examples and Counterexamples.py
+++ week4/Examples and Counterexamples.py
@@ -1,3 +1,21 @@
 for n in range(100):
     if int(str(2**n)[:2])==65:
         print(f'2**{n}={2**n}')
+
+
+def is_prime(n):
+    assert n>0
+    if n==1:
+        return False
+
+    for d in range(2,n):
+        if n%d==0:
+            return False
+
+    return True
+
+for n in range(1,100):
+    if not is_prime(n**2+n+41):
+        print(n)
+        exit()
+
