4de826840797217b4376715b376bfdf9292bb04c
A8250092 <stanley.mei08@gmail.com>
2023-10-24 21:34:16 +0800

    Update Examples and Counterexamples.py

--- week4/Examples and Counterexamples.py
+++ week4/Examples and Counterexamples.py
@@ -1,3 +1,6 @@
+from itertools import combinations
+
+
 for n in range(100):
     if int(str(2**n)[:2])==65:
         print(f'2**{n}={2**n}')
@@ -14,13 +17,18 @@ def is_prime(n):
 
     return True
 
-for n in range(1,100):
-    if not is_prime(n**2+n+41):
-        print(n)
-        exit()
+# for n in range(1,100):
+#     if not is_prime(n**2+n+41):
+#         print(n)
+#         exit()
 
 
 def is_prime2(n):
     return n!=1 and all(n%d!=0 for d in range(2,n))
 
 print(next(n for n in range(1,100) if not is_prime2(n**2+n+41)))
+
+
+for a,b,c in combinations(range(1,20),3):
+    if a**2+b**2==c**2:
+        print(f'{a}**2+{b}**2={c**2}')
