e68809e1d6410a72152f14b2ec72dd1c7134f1de
A8250092 <stanley.mei08@gmail.com>
2023-12-06 16:40:58 +0800

    Update Logic.py

--- week4/Logic.py
+++ week4/Logic.py
@@ -2,3 +2,12 @@ print(3<5 and 7<5)
 print(3<5 and not(7<5))
 print(2+2==5 or 2+2==4)
 
+def foo():
+    print("Foo!")
+    return True
+
+
+if 2+2==5 and foo():
+    print("True")
+else:
+    print("False")
