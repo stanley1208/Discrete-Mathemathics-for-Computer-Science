58c0d32a85bfaa563455f95f90fb98f3fe902122
A8250092 <stanley.mei08@gmail.com>
2023-12-15 17:46:39 +0800

    Update Logic.py

--- week4/Logic.py
+++ week4/Logic.py
@@ -17,3 +17,13 @@ else:
 # if ~((x>9) | (y>3)):
 
 
+a1=[6,2,4]
+print(all((i%2==0) for i in a1))
+a2=[2,7,6]
+print(all((i%2==0) for i in a2))
+
+a3=[1,7,9]
+print(any((i%2==0) for i in a3))
+a4=[9,2,3]
+print(any((i%2==0) for i in a4))
+
